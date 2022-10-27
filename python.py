50122ad4ae6612a841436a5166afae57e40237e8
whayneilao <113647774+whayneilao@users.noreply.github.com>
2022-10-27 12:39:57 +0800

    Create python.py

---
+++ python.py
@@ -0,0 +1,50 @@
+first_name = "Aki"
+favorite_language = "Python"
+
+print(f"Hi, I'm {first_name}. I'm learning {favorite_language}.")
+
+
+PHYTHON FUNCTION:
+def my_function(haikyu):
+  print(haikyu)
+
+my_function(" Kageyama")
+my_function(" Hinata")
+my_function(" Asahi")
+
+LIST:
+haikyulist = ["kageyma", "hinata", "asahi"]
+print(haikyulist)
+
+ADD LIST:
+haikyulist = ["asahi", "hinata", "kageyama"]
+haikyulist.append("sugwara")
+print(haikyulist)
+
+SET:
+haikyuset = {"asahi", "hinata", "kageyama"}
+print(haikyuset)
+
+CONDITION:
+h = 9
+k = 10
+if k > h:
+  print("k is greater than h")
+
+ELIF:
+a = 24
+s = 33
+if s > a:
+  print("s is greater than a")
+elif a == s:
+  print("a and s are equal")
+
+ELSE:
+a = 485
+s = 86
+if s > a:
+  print("s is greater than a")
+elif a == s:
+  print("a and s are equal")
+else:
+  print("a is greater than s")
